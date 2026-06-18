930e38775829e1c627aebf984222a3748f03eb2c
Benyamin50 <TU_EMAIL@github.com>
2026-06-17 21:19:24 -0400

    Commit inicial: Ejercicios Docker 1-3

---
+++ docker_3/app.py
@@ -0,0 +1,16 @@
+﻿# -*- coding: utf-8 -*-
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "¡Hola el hack 3 funciona!"
+
+@app.route('/app')
+def health():
+    return {"status": "ok", "app": "flask app"}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=False)
